775c8ed7b9412b9d40661a57647fd9870b72f0f7
Viacheslav Onofriichuk <zipz211_ova@student.ztu.edu.ua>
2022-12-02 22:44:50 +0200

    1-3 lines changed

--- Task1/Program.cs
+++ Task1/Program.cs
@@ -1,6 +1,6 @@
 ﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Курси Sana Commerce Software Development Bootcamp");
-Console.WriteLine("Курси Sana Commerce Software Development Bootcamp");
-Console.WriteLine("Курси Sana Commerce Software Development Bootcamp");
+Console.WriteLine("Vyacheslav Onofriychuk");
+Console.WriteLine("Vyacheslav Onofriychuk");
+Console.WriteLine("Vyacheslav Onofriychuk");
 Console.WriteLine("Курси Sana Commerce Software Development Bootcamp");
 Console.WriteLine("Курси Sana Commerce Software Development Bootcamp");
